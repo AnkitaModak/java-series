c13a77965de7875df191cdaa89b9872959c736cf
Ankita <ankita180206@gmail.com>
2026-09-09 16:43:32 +0530

    Create CenteredPyramid.java

---
+++ PatternPrinting/src/CenteredPyramid.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CenteredPyramid {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+
+            // Print spaces
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
